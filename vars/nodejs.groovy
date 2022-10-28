eca350c1fa0f9645f94bbfd11d538d53306e030c
vykunta <mudragalla96@gmail.com>
2022-10-28 10:36:25 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,14 +1,53 @@
 def call() {
-    node {
+  node {
 
-        sh 'env'
-        common.codeQuality()
+      common.codeQuality()
 
-        if ( BRANCH_NAME == main || tag ==~ "*" ) {
-            stage('Style Checks') {
-                echo 'Style Checks'
-            }
-        }
+//      stage('Style Checks') {
+//          when {
+//              branch 'main'
+//                }
+//          steps {
+//              echo 'Style Checks'
+//                }
+//            }
+//
+//            stage('Unit Tests') {
+//                when {
+//                    anyOf {
+//                        branch 'main'
+//                        tag "*"
+//                    }
+//                }
+//                steps {
+//                    echo 'Unit Tests'
+//                }
+//            }
+//
+//            stage('Download Dependencies') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Download Dependencies'
+//                }
+//            }
+//
+//            stage('Prepare Artifact') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Prepare Artifact'
+//                }
+//            }
+//
+//            stage('Publish Artifact') {
+//                when { tag "*" }
+//                steps {
+//                    echo 'Publish Artifact'
+//                }
+//            }
+
+
+
+
     }
 
 }

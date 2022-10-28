dda7625f05c409dd4c2b31cbfa25dd491695025f
vykunta <mudragalla96@gmail.com>
2022-10-28 11:22:10 +0530

    jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,13 @@
 def call() {
   node {
 
+      if ( ! env.TAG_NAME ) {
+          env.TAG_NAME = ""
+      }
+
+      sh 'env'
       common.codeQuality()
-      if ( BRANCH_NAME == "main" || tag ==~ "*" ) {
+      if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
         stage('Style Checks') {
             echo 'Style Checks'
         }

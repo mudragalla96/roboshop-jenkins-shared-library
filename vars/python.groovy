9661ee18d20e306e84a95e451155d30ba1fb0d01
vykunta <mudragalla96@gmail.com>
2022-10-29 14:59:27 +0530

    jenkins

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,51 +1,9 @@
 def call() {
-    pipeline {
-
-        agent any
-
-        stages {
-
-
-
-            stage('Style Checks') {
-                when {
-                    branch 'main'
-                }
-                steps {
-                    echo 'Style Checks'
-                }
-            }
-
-            stage('Unit Tests') {
-                when {
-                    anyOf {
-                        branch 'main'
-                        tag "*"
-                    }
-                }
-                steps {
-                    echo 'Unit Tests'
-                }
-            }
-
-            stage('Prepare Artifact') {
-                when { tag "*" }
-                steps {
-                    echo 'Prepare Artifact'
-                }
-            }
-
-            stage('Publish Artifact') {
-                when { tag "*" }
-                steps {
-                    echo 'Publish Artifact'
-                }
-            }
-
-
-        }
-
+    node {
+        common.codeCheckout()
+        common.codeQuality()
+        common.codeChecks()
+        common.artifacts()
     }
-
 
 }

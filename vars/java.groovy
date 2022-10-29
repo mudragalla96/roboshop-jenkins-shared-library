e70b08fb69633fc8f7741abf574f46ed74677696
vykunta <mudragalla96@gmail.com>
2022-10-29 15:24:04 +0530

    jenkins

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,6 +1,10 @@
 def call() {
+    env.SONAR_EXTRA_OPTS = "-Dsonar.java.binaries=./target"
     node {
         common.codeCheckout()
+        stage('Compile Code') {
+            sh 'mvn compile'
+        }
         common.codeQuality()
         common.codeChecks()
         common.artifacts()
